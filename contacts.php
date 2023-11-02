7ffcc561aeb2bd4662da9ff37332f6697e0ea5fc
DESKTOP-IM7VQ8L\SSS <didonikolovv01@gmail.com>
2023-11-02 12:35:17 +0200

    Initial commit

---
+++ contacts.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacts</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="/index.php">Начало</a></li>
+        </ul>
+    </nav>
+    <ul>
+        <li>08950021321</li>
+        <li>230123012312</li>
+    </ul>
+</body>
+</html>
